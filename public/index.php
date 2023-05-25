1312c86ff104b17c77915236035df2b5cec3570d
Aleksey-Duv <alexey.duvanov@inlinegroup-c.ru>
2023-05-25 12:21:13 +0300

    1

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+phpinfo();
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
