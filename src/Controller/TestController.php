f917def6eeca48cdc018338d8218edf91171b2c6
Aleksey-Duv <alexey.duvanov@inlinegroup-c.ru>
2023-05-25 09:43:50 +0300

    1

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use ContainerWGrMk2V\getDoctrine_DatabaseDropCommandService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,15 +14,15 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class TestController extends AbstractController
 {
-    private $user;
-    private $manager;
-
-    public function __construct(entityManagerInterface $manager, UserRepository $user)
-    {
-        $this->manager = $manager;
-        $this->user = $user;
-
-    }
+//    private $user;
+//    private $manager;
+//
+//    public function __construct(entityManagerInterface $manager, UserRepository $user)
+//    {
+//        $this->manager = $manager;
+//        $this->user = $user;
+//
+//    }
     #[Route('/test', name: 'app_test')]
     public function index(): JsonResponse
     {
@@ -55,15 +56,16 @@ class TestController extends AbstractController
         return $response;
 
     }
-    #[Route('/userCreate', name: 'user_create', methods: 'POST')]
+    #[Route('/api/userCreate', name: 'user_create', methods: 'POST')]
     public function userCreate(Request $request, UserPasswordHasherInterface $passwordHash, entityManagerInterface $manager): JsonResponse
     {
+       // dd($request);
         $data = json_decode($request->getContent(), true);
         $email = $data['email'];
         $password = $data['password'];
 
 
-        $email_exist = $this->user->findOneByEmail($email); //как это работает хз, надо разбираться, но работает
+      //  $email_exist = $this->user->findOneByEmail($email); //как это работает хз, надо разбираться, но работает
         $email_exist1 = $manager->getRepository(User::class)->findOneBy(['email' => $email]);
 
         if ($email_exist1) {
@@ -87,8 +89,10 @@ class TestController extends AbstractController
                 ->setRoles(array('ROLE_ADMIN'))
                 ->setPassword($hashedPassword);
 
-            $this->manager->persist($user);
-            $this->manager->flush();
+//            $this->manager->persist($user);
+//            $this->manager->flush();
+            $manager->persist($user);
+            $manager->flush();
             return new JsonResponse
             (
                 [
